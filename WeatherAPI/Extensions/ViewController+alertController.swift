672116576b20b4dee7205eae395a2df68773431d
EvgeniiDb <doktor-dell@mail.ru>
2022-04-23 17:51:30 +0300

    Commit 1 (before API)

---
+++ WeatherAPI/Extensions/ViewController+alertController.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  WeatherAPI
+//
+//  Created by Евгений Доброволец on 23.04.2022.
+//
+
+import Foundation
+import UIKit
+
+extension ViewController {
+    func presentSearchAlertController(withTitle title: String?, message: String?, style: UIAlertController.Style) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: style)
+        ac.addTextField { tf in
+            let cities = ["San Francisco", "Moscow", "New York", "Stambul", "Viena"]
+            tf.placeholder = cities.randomElement()
+        }
+        let search = UIAlertAction(title: "Search", style: .default) { action in
+            let textField = ac.textFields?.first
+            guard let cityName = textField?.text else { return }
+            if cityName != "" {
+                print("search info for the \(cityName)")
+            }
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        ac.addAction(search)
+        ac.addAction(cancel)
+        present(ac, animated: true, completion: nil)
+        
+    }
+}
---
+++ WeatherAPI/View Controllers/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  WeatherAPI
+//
+//  Created by Евгений Доброволец on 23.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
